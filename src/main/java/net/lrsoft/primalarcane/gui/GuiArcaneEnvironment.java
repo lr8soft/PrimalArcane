a0f09b3a3ff2f9aa081e754a2e52aa94a3dc89a3
LT_lrsoft <lrsoft@hotmail.com>
2021-12-29 01:31:01 +0800

    v0.13

--- src/main/java/net/lrsoft/primalarcane/gui/GuiArcaneEnvironment.java
+++ src/main/java/net/lrsoft/primalarcane/gui/GuiArcaneEnvironment.java
@@ -1,17 +1,29 @@
 package net.lrsoft.primalarcane.gui;
 
+import org.lwjgl.opengl.GL11;
+
 import net.lrsoft.primalarcane.PrimalArcane;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.util.ResourceLocation;
 
 public class GuiArcaneEnvironment extends Gui
 {
+	private static ResourceLocation hudTexture = new ResourceLocation(PrimalArcane.MODID,"textures/gui/hud.png");
+	private static final int texWidth = 384, texHeight = 384;
+	private static final float designWidth = 640f, designHeight = 480f;
+	
     String text = "PrimalArcane INDEV";
     
     static float postiveMana = 0.0f;
     static float negativeMana = 0.0f;
     static float maxMana = 0.0f;
+    
     public GuiArcaneEnvironment(Minecraft mc)
     {
         ScaledResolution scaled = new ScaledResolution(mc);
@@ -20,18 +32,88 @@ public class GuiArcaneEnvironment extends Gui
  
         drawString(mc.fontRenderer, text, 30, 30, Integer.parseInt("FFFFFF", 16));
         
-        StringBuilder builder = new StringBuilder();
-        builder.append("+:");
-        builder.append(postiveMana);
-        builder.append(" -:");
-        builder.append(negativeMana);
-        builder.append(" max:");
-        builder.append(maxMana);
         
-        String result = builder.toString();
+        int startX = 20;
+        int startY = 20;
         
-        drawString(mc.fontRenderer, result, 30, 60, Integer.parseInt("FFAA00", 16));
+        float xScale = 0.5f;
+        float yScale = 0.4f;
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        mc.renderEngine.bindTexture(hudTexture);
+        
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_CLAMP);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_CLAMP);
+        
+        // render mana progress frame
+        {
+
+    		GlStateManager.pushMatrix();
+    		
+    		float translateY = (height - yScale * texHeight) / 2.0f;
+            GlStateManager.translate(startX, translateY, this.zLevel);
+            GlStateManager.scale(xScale, yScale, 0.5f);
+
+            this.drawModalRectWithCustomSizedTexture(0, 0, 0.0f, 0.0f, 20, texHeight, texWidth, texHeight);
+            GlStateManager.popMatrix();
+        }
+        // render postive mana
+        {
+        	float postiveRate = postiveMana / maxMana;
+        	int postiveTexHeight = Math.round(postiveRate * (float)texWidth);
+        	
+    		GlStateManager.pushMatrix();
+    		
+    		float translateY = (height - yScale * texHeight) / 2.0f;
+            GlStateManager.translate(startX, translateY, this.zLevel);
+            GlStateManager.scale(xScale, yScale, 0.5f);
+            
+            this.drawModalRectWithCustomSizedTextureReverb(0, texWidth - postiveTexHeight,
+            		20.0f,  0.0f,
+            		20, postiveTexHeight,
+            		texWidth, texHeight);
+            
+            GlStateManager.popMatrix();
+        }
+        
+        // render negative mana
+        {
+        	float negativeRate = negativeMana / maxMana;
+        	int negativeTexHeight = Math.round(negativeRate * (float)texWidth);
+        	
+    		GlStateManager.pushMatrix();
+    		
+    		float translateY = (height - yScale * texHeight) / 2.0f;
+            GlStateManager.translate(startX, translateY, this.zLevel);
+            GlStateManager.scale(xScale, yScale, 0.5f);
+    		
+            this.drawModalRectWithCustomSizedTexture(0, 0,
+            		38.0f,  0.0f,
+            		20, negativeTexHeight,
+            		texWidth, texHeight);
+
+            GlStateManager.popMatrix();
+        }
+        
+
+
+
     }
+    
+    public static void drawModalRectWithCustomSizedTextureReverb(int x, int y, float u, float v, int width, int height, float textureWidth, float textureHeight)
+    {
+        float f = 1.0F / textureWidth;
+        float f1 = 1.0F / textureHeight;
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder bufferbuilder = tessellator.getBuffer();
+        bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
+        bufferbuilder.pos((double)x, (double)(y + height), 0.0D).tex((double)(u * f), (double)(v * f1)).endVertex();
+        bufferbuilder.pos((double)(x + width), (double)(y + height), 0.0D).tex((double)((u + (float)width) * f), (double)(v * f1)).endVertex();
+        bufferbuilder.pos((double)(x + width), (double)y, 0.0D).tex((double)((u + (float)width) * f), (double)((v + (float)height) * f1)).endVertex();
+        bufferbuilder.pos((double)x, (double)y, 0.0D).tex((double)(u * f), (double)((v + (float)height) * f1)).endVertex();
+        tessellator.draw();
+    }
+    
+    
     
     public static void updateManaInfo(float p, float n, float max) {
     	postiveMana = p;
